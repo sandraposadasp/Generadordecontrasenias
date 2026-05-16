29d75876f788aac20b2dcb0c695d2850fc6577d5
Sandra <hi-sandra@hotmail.com>
2026-05-16 15:03:12 -0600

    elementos del DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+const passwordInput = document.getElementById("password");
+const longitudInput = document.getElementById("longitud");
+const longitudValor = document.getElementById("longitudValor");
+const nivelTexto = document.getElementById("nivel");
+
+const mayusculas = document.getElementById("mayusculas");
+const minusculas = document.getElementById("minusculas");
+const numeros = document.getElementById("numeros");
+const simbolos = document.getElementById("simbolos");
+
+const botonGenerar = document.getElementById("generar");
+const botonCopiar = document.getElementById("copiar");
