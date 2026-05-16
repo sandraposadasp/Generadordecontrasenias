ef25f2579951dfc2503ac105af717eb2f9a2df20
Sandra <hi-sandra@hotmail.com>
2026-05-16 15:10:49 -0600

    funcion de generar contraseña

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ const simbolos = document.getElementById("simbolos");
 const botonGenerar = document.getElementById("generar");
 const botonCopiar = document.getElementById("copiar");
 
+//Slider
 longitudInput.addEventListener("input", () => {
     longitudValor.textContent = longitudInput.value;
 });
 
+//caracteres
 function getMayuscula() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
@@ -30,4 +32,31 @@ function getNumero() {
 function getSimbolo() {
     const simbolos = "!@#$%^&*()_+[]{}<>?";
     return simbolos[Math.floor(Math.random() * simbolos.length)];
+}
+
+//generar contraseña
+function generarPassword() {
+    let password = "";
+    const longitud = longitudInput.value;
+
+    const opciones = [];
+
+    if (mayusculas.checked) opciones.push(getMayuscula);
+    if (minusculas.checked) opciones.push(getMinuscula);
+    if (numeros.checked) opciones.push(getNumero);
+    if (simbolos.checked) opciones.push(getSimbolo);
+
+    if (opciones.length === 0) {
+        alert("Selecciona al menos una opción");
+        return;
+    }
+
+    for (let i = 0; i < longitud; i++) {
+        const random = opciones[Math.floor(Math.random() * opciones.length)];
+        password += random();
+    }
+
+    passwordInput.value = password;
+    
+    evaluarSeguridad(password);
 }
