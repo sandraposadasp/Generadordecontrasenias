bb13a44a94aefc1d401769bc68189ebb095af003
Sandra <hi-sandra@hotmail.com>
2026-05-16 15:08:13 -0600

    funciones de los carcateres

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ const botonCopiar = document.getElementById("copiar");
 longitudInput.addEventListener("input", () => {
     longitudValor.textContent = longitudInput.value;
 });
+
+function getMayuscula() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getMinuscula() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getNumero() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getSimbolo() {
+    const simbolos = "!@#$%^&*()_+[]{}<>?";
+    return simbolos[Math.floor(Math.random() * simbolos.length)];
+}
